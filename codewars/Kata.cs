4ff8c400778c7a85d04d75c10effb94cbd7f715d
Rail Latypov <v-ralaty@microsoft.com>
2021-01-12 18:34:44 +0300

    Added one method

--- codewars/Kata.cs
+++ codewars/Kata.cs
@@ -1168,6 +1168,31 @@ namespace Codewars
             return number.ToString().Select(q => int.Parse(new string(q, 1))).ToArray();
          
         }
+
+
+        //get the most frequent element in array
+        public static int GetMostFrequentElement(int[] newarr)
+        {
+            var dic = new Dictionary<int, int>();
+            for (int i = 0; i < newarr.Length; i++)
+            {
+                if (!dic.ContainsKey(newarr[i]))
+                    dic.Add(newarr[i], 1);
+                else
+                    dic[newarr[i]]++;
+            }
+            int key = dic.FirstOrDefault(x => x.Value == dic.Values.Max()).Key;
+
+            /*
+            var list = new List<int>();
+            foreach(var el in newarr)
+            {
+                list.Add(el);
+            }
+            var most = list.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
+            */
+            return key;
+        }
     }
 
 }
--- codewars/Program.cs
+++ codewars/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Codewars
@@ -9,9 +10,27 @@ namespace Codewars
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-            Kata.PlusOne(arr);
-            Console.ReadKey();
+            int[] newarr = new int[] { 1, 2, 0, 44, 3, 3 };
+            
+            var dic = new Dictionary<int, int>();
+            for (int i = 0; i < newarr.Length; i++)
+            {
+                if (!dic.ContainsKey(newarr[i]))
+                    dic.Add(newarr[i], 1);
+                else
+                    dic[newarr[i]]++;
+            }
+            int key = dic.FirstOrDefault(x => x.Value == dic.Values.Max()).Key;
+
+            /*
+            var list = new List<int>();
+            foreach(var el in newarr)
+            {
+                list.Add(el);
+            }
+            var most = list.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
+            */
+            Console.WriteLine(key);
         }
     }
 }
